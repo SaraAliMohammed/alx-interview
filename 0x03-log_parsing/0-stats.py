4a737f6b9d8cf9cac863f3bfcb6d564dc13d5c8d
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-05-15 12:35:09 +0000

    Task0. Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Module that reads stdin line by line and computes metrics """
+import sys
+
+
+def print_msg(codes, file_size):
+    ''' print message function '''
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
+file_size = 0
+code = 0
+lines_count = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
+
+        if len(parsed_line) > 2:
+            lines_count += 1
+
+            if lines_count <= 10:
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
+
+                if (code in codes.keys()):
+                    codes[code] += 1
+
+            if (lines_count == 10):
+                print_msg(codes, file_size)
+                lines_count = 0
+
+finally:
+    print_msg(codes, file_size)
